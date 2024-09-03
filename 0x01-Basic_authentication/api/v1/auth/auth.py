965940636cb78b2076ac6a607e136466bc5f2621
Abdellahsyani <syaniabdos@gmail.com>
2024-09-03 17:58:29 +0100

    auth action

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,7 +2,7 @@
 """create a class authentication
 """
 from flask import request
-from typing import TypeVar
+from typing import List, TypeVar
 
 class Auth:
     """API authentication
@@ -19,19 +19,12 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """return
         """
-        head = request.headers.get('Authorization')
-        return head
+        if request is not None:
+            return request.headers.get('Authorization')
+        return None
 
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """return
+        """get the current user from the request
         """
-         authorization_header = request.headers.get('Authorization')
-        if authorization_header:
-            token = authorization_header.split(' ')[1]
-            # Validate the token and extract user information
-            user_data = validate_token(token)
-            if user_data:
-                return User(**user_data)  # Assuming User class has appropriate fields
-
         return None
