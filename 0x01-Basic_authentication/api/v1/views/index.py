84da4100fba66814a41d6e02d4955df4b920148b
Abdellahsyani <syaniabdos@gmail.com>
2024-09-03 12:37:50 +0100

    update index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=[GET])
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
 def unouthorized():
     """unouthorized method
     """
