f7e4596f28d499af699ac0bda34ffd218af79263
Abdellahsyani <syaniabdos@gmail.com>
2024-09-03 17:23:33 +0100

    prepare the class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,3 +7,20 @@ from flask import request
 class Auth:
     """API authentication
     """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return
+        """
+        pass
+
+
+    def authorization_header(self, request=None) -> str:
+        """return
+        """
+        pass
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """return
+        """
+        pass
