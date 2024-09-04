9457d47ef10c3b66bf71ab9864efd8a0c72952a0
Abdellahsyani <syaniabdos@gmail.com>
2024-09-04 23:15:58 +0100

    just add

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,13 +9,18 @@ import os
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = Auth()
+auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""in this time the class empty
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """start class"""
+    pass
