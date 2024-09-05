ad9c2ee05955109e8e0f83a93f998f2b71959c81
Abdellahsyani <syaniabdos@gmail.com>
2024-09-05 10:09:03 +0100

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,30 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):]
+
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ return the decoded value of base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        # check for the correct authorization_header
+        if not base64_authorization_header.startswith('Basic '):
+            return None
+
+        # remove the Basic and get decode part
+        decoded_byte = base64_authorization_header[len('Basic '):]
+        try:
+            # convert the decode part to byte
+            code_byte = base64.b64decode(decoded_byte, validate=True)
+
+            # convert byte to utf-8
+            return code_byte.decode('utf-8')
+        except (base64.binascii.Error, ValueError) as error:
+            print("Error decoding base64: {}".format(error))
+            return None
