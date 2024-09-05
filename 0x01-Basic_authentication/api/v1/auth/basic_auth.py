55272b7fb03e8c7534376ce43298ad5409c825db
Abdellahsyani <syaniabdos@gmail.com>
2024-09-05 09:12:40 +0100

    base64 decoded

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """start class"""
-    pass
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """return the base64
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[len('Basic '):]
