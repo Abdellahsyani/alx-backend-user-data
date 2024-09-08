89f6f4b8409d985ed2a985cf7e1c3686892578f7
Abdellahsyani <syaniabdos@gmail.com>
2024-09-08 11:19:33 +0100

    get user id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,3 +19,10 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ return User ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
