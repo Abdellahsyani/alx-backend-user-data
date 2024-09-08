9047bc979f323cc7e9f39e7648f15ac2fdaf232a
Abdellahsyani <syaniabdos@gmail.com>
2024-09-08 11:07:22 +0100

    create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,20 @@
 """start a new authentication mechanism
 """
 from api.v1.auth.auth import Auth
+from flask import Flask, session
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ authentication mechanism starting
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a session id for user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
