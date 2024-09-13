a38e3251e90909c156eed33084d4a15c4de34077
Abdellahsyani <syaniabdos@gmail.com>
2024-09-13 22:28:43 +0100

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """return a user object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
